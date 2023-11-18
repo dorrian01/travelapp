076993ebc54c5f8ca514faf3bb89f17cdea150eb
giorgi vashakmadze <giorgi.vashaqmadze666@gmail.com>
2023-11-19 00:31:38 +0400

    fix but for deployment

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Camp from "@/components/Camp";
 import Features from "@/components/Features";
 import GetApp from "@/components/GetApp";
 import Guide from "@/components/Guide";
-import Hero from "@/components/hero";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
